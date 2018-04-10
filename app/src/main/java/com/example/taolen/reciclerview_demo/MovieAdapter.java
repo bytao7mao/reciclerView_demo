a3122a905c7ae180ddc7528d4052793fd67a1da4
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2018-04-11 00:57:44 +0300

    first chapter of this MONSTER: RECICLERVIEW

---
+++ app/src/main/java/com/example/taolen/reciclerview_demo/MovieAdapter.java
@@ -0,0 +1,49 @@
+package com.example.taolen.reciclerview_demo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by taoLen on 4/10/2018.
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+    private List<Movie> movieList;
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, year, genre;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = view.findViewById(R.id.title);
+            genre = view.findViewById(R.id.genre);
+            year = view.findViewById(R.id.year);
+        }
+    }
+    public MovieAdapter(List<Movie> movieList) {this.movieList = movieList;}
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Movie movie = movieList.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.genre.setText(movie.getGenre());
+        holder.year.setText(movie.getYear());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+}
